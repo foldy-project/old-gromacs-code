7426537a789dea846622c2ec97b5b935c6fcaadf
Tom Havlik <keres993@gmail.com>
2020-03-02 11:16:48 -0600

    untested normalization code

---
+++ client/normalize.py
@@ -0,0 +1,241 @@
+from Bio.PDB.Residue import Residue
+from Bio.PDB.Chain import Chain
+from Bio.PDB.Atom import Atom
+
+def get_atoms_from_desc(residue: Residue, description: list):
+    atoms = []
+    for i, (element, id) in enumerate(description):
+        if not id in residue.child_dict:
+            raise ValueError(f'atom "{id}" not found')
+        atom = residue.child_dict[id]
+        if atom.element != element:
+            raise ValueError(
+                f'expected element "{element}" from atom {i}, got element "{atom.element}"')
+        atoms.append(atom)
+    return atoms
+
+
+class NormalizedResidue:
+    def __init__(self, residue: Residue, description: list):
+        self.residue = Residue(residue.id, residue.resname, residue.segid)
+        atoms = get_atoms_from_desc(residue, description)
+        for atom in atoms:
+            self.residue.add(atom.copy())
+
+
+class Ala(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB')]
+        super(Ala, self).__init__(residue, description)
+
+
+class Arg(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('C', 'CG'),
+                       ('C', 'CD'), ('N', 'NE'), ('C', 'CZ'),
+                       ('N', 'NH1'), ('N', 'NH2')]
+        super(Arg, self).__init__(residue, description)
+
+
+class Asn(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('C', 'CG'),
+                       ('O', 'OD1'), ('N', 'ND2')]
+        super(Asn, self).__init__(residue, description)
+
+
+class Asp(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('C', 'CG'),
+                       ('O', 'OD1'), ('O', 'OD2')]
+        super(Asp, self).__init__(residue, description)
+
+
+class Cys(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('S', 'SG')]
+        super(Cys, self).__init__(residue, description)
+
+
+class Gln(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('C', 'CG'),
+                       ('C', 'CD'), ('O', 'OE1'), ('N', 'NE2')]
+        super(Gln, self).__init__(residue, description)
+
+
+class Glu(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('C', 'CG'),
+                       ('C', 'CD'), ('O', 'OE1'), ('O', 'OE2')]
+        super(Glu, self).__init__(residue, description)
+
+
+class Gly(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'), ('O', 'O')]
+        super(Gly, self).__init__(residue, description)
+
+
+class His(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('C', 'CG'),
+                       ('N', 'ND1'), ('C', 'CD2'), ('C', 'CE1'),
+                       ('N', 'NE2')]
+        super(His, self).__init__(residue, description)
+
+
+class Ile(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('C', 'CG1'),
+                       ('C', 'CG2'), ('C', 'CD1')]
+        super(Ile, self).__init__(residue, description)
+
+
+class Leu(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('C', 'CG'),
+                       ('C', 'CD1'), ('C', 'CD2')]
+        super(Leu, self).__init__(residue, description)
+
+
+class Lys(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('C', 'CG'),
+                       ('C', 'CD'), ('C', 'CE'), ('N', 'NZ')]
+        super(Lys, self).__init__(residue, description)
+
+
+class Met(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('C', 'CB'), ('C', 'CG'), ('S', 'SD'),
+                       ('C', 'CE')]
+        super(Met, self).__init__(residue, description)
+
+
+class NH2(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N')]
+        super(NH2, self).__init__(residue, description)
+
+
+class Phe(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('C', 'CG'),
+                       ('C', 'CD1'), ('C', 'CD2'), ('C', 'CE1'),
+                       ('C', 'CE2'), ('C', 'CZ')]
+        super(Phe, self).__init__(residue, description)
+
+
+class Pro(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('C', 'CG'),
+                       ('C', 'CD')]
+        super(Pro, self).__init__(residue, description)
+
+
+class Ser(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('O', 'OG')]
+        super(Ser, self).__init__(residue, description)
+
+
+class Thr(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('O', 'OG1'),
+                       ('C', 'CG2')]
+        super(Thr, self).__init__(residue, description)
+
+
+class Trp(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('C', 'CG'),
+                       ('C', 'CD1'), ('C', 'CD2'), ('N', 'NE1'),
+                       ('C', 'CE2'), ('C', 'CE3'), ('C', 'CZ2'),
+                       ('C', 'CZ3'), ('C', 'CH2')]
+        super(Trp, self).__init__(residue, description)
+
+
+class Tyr(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('C', 'CG'),
+                       ('C', 'CD1'), ('C', 'CD2'), ('C', 'CE1'),
+                       ('C', 'CE2'), ('C', 'CZ'), ('O', 'OH')]
+        super(Tyr, self).__init__(residue, description)
+
+
+class Val(NormalizedResidue):
+    def __init__(self, residue: Residue):
+        description = [('N', 'N'), ('C', 'CA'), ('C', 'C'),
+                       ('O', 'O'), ('C', 'CB'), ('C', 'CG1'),
+                       ('C', 'CG2')]
+        super(Val, self).__init__(residue, description)
+
+
+_classes = {
+    'PRO': Pro,
+    'MET': Met,
+    'ASP': Asp,
+    'VAL': Val,
+    'SER': Ser,
+    'HIS': His,
+    # 'NH2': NH2,
+    'GLU': Glu,
+    'GLN': Gln,
+    'ASN': Asn,
+    'GLY': Gly,
+    'ILE': Ile,
+    'LYS': Lys,
+    'TRP': Trp,
+    'TYR': Tyr,
+    'THR': Thr,
+    'ARG': Arg,
+    'PHE': Phe,
+    'CYS': Cys,
+    'LEU': Leu,
+    'ALA': Ala,
+}
+
+def get_residue_class(resname: str) -> object:
+    if not resname in _classes:
+        raise ValueError(f'unknown resname "{resname}"')
+    return _classes[resname]
+
+def normalize_residue(residue: Residue) -> Residue:
+    ty = get_residue_class(residue.resname)
+    obj = ty(residue)
+    return obj.residue
+
+def normalize_chain(chain: Chain, ignore_residues: set) -> Chain:
+    new_chain = Chain(chain.id)
+    for residue in chain:
+        if residue.resname in ignore_residues:
+            print(f'Ignoring residue {residue.resname}')
+            continue
+        new_chain.add(normalize_residue(residue))
+    return new_chain
+
+if __name__ == '__main__':
+    from simulate import normalize_structure
+    ignore_residues = set([
+        'HOH',
+    ])
+    normalize_structure('pdb1aki.ent', pdb_id='1aki', model_id=0, chain_id='A', ignore_residues=ignore_residues)
--- client/simulate.py
+++ client/simulate.py
@@ -21,6 +21,7 @@ from Bio.PDB.Model import Model
 from Bio.PDB.Residue import Residue
 from Bio.PDB.Chain import Chain
 from Bio.PDB.Atom import Atom
+from normalize import normalize_chain
 
 script_dir = os.path.dirname(sys.argv[0])
 
@@ -31,16 +32,18 @@ flags.DEFINE_string("bucket", 'pdb', 'S3 bucket name')
 flags.DEFINE_string("endpoint", 'https://sfo2.digitaloceanspaces.com',
                     "S3 endpoint")
 flags.DEFINE_string("pdb_id", None, "pdb structure ID")
+flags.DEFINE_string("model_id", None, "model ID")
+flags.DEFINE_string("chain_id", None, "chain ID")
 flags.DEFINE_string("correlation_id", None, "correlation ID")
-flags.DEFINE_string("foldy_operator_host",
-                    'foldy-operator', "foldy operator host")
+flags.DEFINE_string("foldy_operator_host", 'foldy-operator',
+                    "foldy operator host")
 flags.DEFINE_integer("foldy_operator_port", 8090, "foldy operator port")
 flags.DEFINE_float(
-    "emtol", 10.0, "stop minimization when the maximum force below this many kJ")
+    "emtol", 10.0,
+    "stop minimization when the maximum force below this many kJ")
 flags.DEFINE_float("emstep", 0.01, "minimization step size")
 flags.DEFINE_integer("nsteps", 1000, "simulation steps")
 flags.DEFINE_float("dt", 0.0002, "simulation time step")
-
 
 
 class PDBNotFoundException(Exception):
@@ -54,8 +57,9 @@ class PDBNotFoundException(Exception):
 
 def report_error(msg: str):
     print('Reporting error: {}'.format(msg))
-    conn = http.client.HTTPConnection(
-        FLAGS.foldy_operator_host, FLAGS.foldy_operator_port, timeout=10)
+    conn = http.client.HTTPConnection(FLAGS.foldy_operator_host,
+                                      FLAGS.foldy_operator_port,
+                                      timeout=10)
     json_data = json.dumps({
         'msg': msg,
         'correlation_id': FLAGS.correlation_id,
@@ -64,27 +68,65 @@ def report_error(msg: str):
     conn.request('POST', '/error', json_data, headers)
     response = conn.getresponse()
     if response.code != 200:
-        raise ValueError(
-            'error report: expected code 200, got {}'.format(response.code))
+        raise ValueError('error report: expected code 200, got {}'.format(
+            response.code))
+
+
+def normalize_structure(input_path: str,
+                        pdb_id: str,
+                        model_id: int,
+                        chain_id: str,
+                        ignore_residues: set):
+    with warnings.catch_warnings(record=True):
+        warnings.simplefilter("ignore", PDBConstructionWarning)
+        parser = PDBParser()
+        structure = parser.get_structure(pdb_id, input_path)
+        if not model_id in structure.child_dict:
+            raise ValueError(f'model "{model_id}" not found in "{pdb_id}"')
+        model = structure.child_dict[model_id]
+        if not chain_id in model.child_dict:
+            raise ValueError(
+                f'chain "{chain_id}" not found in "{pdb_id}" model "{model_id}"')
+        chain = model.child_dict[chain_id]
+
+        new_chain = normalize_chain(chain, ignore_residues=ignore_residues)
+        new_model = Model(model.id)
+        new_model.add(new_chain)
+        new_structure = Structure(structure.id)
+        new_structure.add(new_model)
+
+        out_path = input_path + '.norm'
+        io = PDBIO()
+        io.set_structure(new_structure)
+        io.save(out_path)
+        print(f'Normalized {pdb_id} to {out_path}')
+        return out_path
 
 
 def prepare_input_data(pdb_id: str,
-                       correlation_id: str) -> str:
+                       model_id: str,
+                       chain_id: str,
+                       ignore_residues: set) -> str:
     s3 = boto3.resource('s3',
                         region_name=FLAGS.region,
                         endpoint_url=FLAGS.endpoint)
     bucket = s3.Bucket(name=FLAGS.bucket)
+    path = '/tmp/pdb{}.ent.gz'.format(pdb_id)
     try:
         object = bucket.Object('pdb{}.ent.gz'.format(pdb_id))
-        with open('/tmp/pdb{}.ent.gz'.format(pdb_id), 'wb') as f:
+        with open(path, 'wb') as f:
             object.download_fileobj(f)
     except botocore.exceptions.ClientError:
         _, value, _ = sys.exc_info()
         if value.response['Error']['Code'] == '404':
             raise PDBNotFoundException(pdb_id)
         raise
-    run_cmd(['gzip', '-df', '/tmp/pdb{}.ent.gz'.format(pdb_id)])
-    return '/tmp/pdb{}.ent'.format(pdb_id)
+    run_cmd(['gzip', '-df', path])
+    return normalize_structure(path,
+                               pdb_id=pdb_id,
+                               model_id=model_id,
+                               chain_id=chain_id,
+                               ignore_residues=ignore_residues)
 
 
 def run_cmd(args, expect_exitcode=0):
@@ -97,12 +139,8 @@ def run_cmd(args, expect_exitcode=0):
         raise ValueError(msg)
 
 
-def run_simulation(pdb_id: str,
-                   input_pdb: str,
-                   emtol: float,
-                   emstep: float,
-                   nsteps: int,
-                   dt: float):
+def run_simulation(pdb_id: str, input_pdb: str, emtol: float, emstep: float,
+                   nsteps: int, dt: float):
     # TODO
     # it is unclear why this number needs to be ~5x
     # larger in order to generate enough frame data.
@@ -110,66 +148,70 @@ def run_simulation(pdb_id: str,
     # really care about wasting a bit of compute.
     nsteps *= 5
 
-    run_cmd(['./run-simulation.sh',
-             pdb_id,
-             input_pdb,
-             str(emtol),
-             str(emstep),
-             str(nsteps),
-             str(dt)])
+    run_cmd([
+        './run-simulation.sh', pdb_id, input_pdb,
+        str(emtol),
+        str(emstep),
+        str(nsteps),
+        str(dt)
+    ])
     return 0
 
 
 def trjconv(pdb_id: str, nsteps: int):
-    with warnings.catch_warnings(record=True):
-        warnings.simplefilter("ignore", PDBConstructionWarning)
-        parser = PDBParser()
-        structures = []
-        for i in range(nsteps):
-            proc = subprocess.run(
-                ['./trjconv.sh', pdb_id, str(i), str(i+1)], stdout=PIPE, stderr=PIPE)
-            if proc.returncode != 0:
-                print('trjconv {}'.format(i))
-                print(str(proc.stdout))
-                print(str(proc.stderr))
-                raise ValueError('error converting frame #{}: {} {}'.format(
-                    i, str(proc.stdout), str(proc.stderr)))
-            path = '{}_minim_{}.pdb'.format(pdb_id, i)
-            assert os.path.isfile(path)
-            #structure = parser.get_structure(pdb_id, path)
-            #structures.append(structure)
-            #os.remove(path) # save memory
-    return structures
+    for i in range(nsteps):
+        proc = subprocess.run(
+            ['./trjconv.sh', pdb_id,
+                str(i), str(i + 1)],
+            stdout=PIPE,
+            stderr=PIPE)
+        if proc.returncode != 0:
+            print('trjconv {}'.format(i))
+            print(str(proc.stdout))
+            print(str(proc.stderr))
+            raise ValueError('error converting frame #{}: {} {}'.format(
+                i, str(proc.stdout), str(proc.stderr)))
+        path = '{}_minim_{}.pdb'.format(pdb_id, i)
+        assert os.path.isfile(path)
+
 
 def upload(pdb_id: str, correlation_id: str):
     run_cmd(['./upload.sh', pdb_id, correlation_id])
     print('Results uploaded')
 
+
 def calc_deltas(structures: List[Structure]):
-    #for i, (a, b) in enumerate(zip(structures[:-1], structures[1:])):
+    # for i, (a, b) in enumerate(zip(structures[:-1], structures[1:])):
     #    for (model_a, model_b) in zip(a, b):
     #        for (chain_a, chain_b) in zip(model_a, model_b):
     #            for (residue_a, residue_b) in zip(chain_a, chain_b):
     #                for (atom_a, atom_b) in zip(residue_a, residue_b):
     #                    if atom_a.element != atom_b.element:
     #                        raise ValueError('element mismatch ({} and {})'.format(atom_a.element, atom_b.element))
-    #                    dcoord = atom_b.coord - atom_a.coord 
+    #                    dcoord = atom_b.coord - atom_a.coord
     #                    print('{} {}'.format(atom_a.element, dcoord))
     print('Done!')
     return None
 
+
 def main(_argv):
+    ignore_residues = set([
+        'HOH',
+    ])
     try:
         if not FLAGS.pdb_id:
             raise ValueError('missing pdb_id')
         if not FLAGS.correlation_id:
             raise ValueError('missing correlation_id')
-        pdb_id = FLAGS.pdb_id.lower() # normalize to lowercase
-        print('Simulating {} for {} steps'.format(pdb_id.upper(), FLAGS.nsteps))
+        pdb_id = FLAGS.pdb_id.lower()  # normalize to lowercase
+        print('Simulating {} for {} steps'.format(pdb_id.upper(),
+                                                  FLAGS.nsteps))
         correlation_id = FLAGS.correlation_id
         print('Preparing input data...')
         input_pdb = prepare_input_data(pdb_id=pdb_id,
-                                       correlation_id=correlation_id)
+                                       model_id=FLAGS.model_id,
+                                       chain_id=FLAGS.chain_id,
+                                       ignore_residues=ignore_residues)
         print('Running simulation...')
         run_simulation(pdb_id=pdb_id,
                        input_pdb=input_pdb,
@@ -178,7 +220,7 @@ def main(_argv):
                        nsteps=FLAGS.nsteps,
                        dt=FLAGS.dt)
         print('Extracting frames...')
-        structures = trjconv(pdb_id, FLAGS.nsteps)
+        trjconv(pdb_id, FLAGS.nsteps)
         #deltas = calc_deltas(structures)
         print('Uploading results...')
         upload(pdb_id, correlation_id)
